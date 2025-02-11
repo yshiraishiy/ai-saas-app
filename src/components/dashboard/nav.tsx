ea4ef7167a95d424c0bc61d62d6c121459f7dc39
yuya.shiraishi <yuya0505soccer@gmail.com>
2025-02-11 15:54:13 +0900

    ✨ダッシュボードにナビゲーションを表示

--- src/components/dashboard/nav.tsx
+++ src/components/dashboard/nav.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+"use client";
+
+import { navItems } from "@/config/nav";
+import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const DashBoardNav = () => {
-  return (
-    <nav>
-      {}
-    </nav>
-  )
-}
+  const pathname = usePathname();
 
-export default DashBoardNav
+  return (
+    <nav className="grid gap-2 items-start">
+      {navItems.map((item) => (
+        <Button
+          key={item.href}
+          variant={pathname === item.href ? "secondary" : "ghost"}
+          className={cn("justify-start", pathname === item.href && "bg-accent")}
+          asChild
+        >
+          <Link href={item.href}>
+            {item.icon && <item.icon className="h-4 w-4 mr-2" />}
+            {item.title}
+          </Link>
+        </Button>
+      ))}
+    </nav>
+  );
+};
+
+export default DashBoardNav;
