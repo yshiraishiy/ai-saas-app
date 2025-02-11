01f8e24053e6bda9afbe57ccdfb36db8afb0ff24
yuya.shiraishi <yuya0505soccer@gmail.com>
2025-02-11 15:01:19 +0900

    ✨ナビゲーションに必要なデータを用意

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import DashBoardNav from "@/components/dashboard/nav";
 import Link from "next/link";
 
 export default function DashboardLayout({
@@ -8,8 +9,8 @@ export default function DashboardLayout({
   return (
     <div>
       {/* header */}
-      <header className="sticky top-0 z-40 border-b bg-background">
-        <div className="container flex items-center h-16 px-4">
+      <header className="sticky top-0 z-40 border-b bg-background px-4">
+        <div className="flex items-center h-16 max-w-screen-xl mx-auto w-full">
           <Link href={"/"}>
             <h1 className="text-lg">AI Image Generator</h1>
           </Link>
@@ -17,14 +18,16 @@ export default function DashboardLayout({
       </header>
 
       {/* dashboard */}
-      <div>
+      <div className="mx-auto w-full max-w-screen-xl md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
         {/* sidebar */}
-        <aside>
-          <div></div>
+        <aside className="md:sticky top-16 z-30 hidden md:block border-r h-[calc(100vh-4rem)]">
+          <div className="py-6 px-2 lg:py-8"><DashBoardNav /></div>
         </aside>
 
         {/* main contents */}
-        <main>{children}</main>
+        <main className="flex w-full flex-col p-4">
+          {children}
+        </main>
       </div>
     </div>
   );
